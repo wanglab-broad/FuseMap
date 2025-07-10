ee3a8f4745fba34b567c8b78335be4c17fcb8233
Yichun He <32154774+yichunher@users.noreply.github.com>
2025-07-09 23:11:37 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md','r') as f:
 
 setup(
     name='fusemap',
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=[
         'scanpy==1.9.3',
@@ -13,7 +13,17 @@ setup(
         'dgl==1.1.1',
         'sparse==0.14.0',
         'leidenalg==0.10.1',
-        'dglgo==0.0.2'
+        'dglgo==0.0.2',
+        "streamlit==1.45.1",
+        "openai==1.82.1",
+        "langchain==0.3.25",
+        "langchain-community==0.3.24",
+        "langchain-openai==0.3.18",
+        "audio-recorder-streamlit==0.0.10",
+        "plotly==6.1.2",
+        "harmonypy==0.0.10",
+        "langgraph==0.4.7",
+        "numpy==1.26.2"
     ],
     entry_points={
         "console_scripts":[
